6bea109a6a803571466c2579ce027c3b565beb09
Tal <tal.yaron@gmail.com>
2021-08-20 10:17:21 +0300

    cont

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+"use strict";
+exports.__esModule = true;
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 app.use(express.static('public'));
+app.use(express.static('public/dist'));
 app.listen(port, function () {
     console.log('app listen on port', port);
 });
--- server.ts
+++ server.ts
@@ -1,9 +1,13 @@
+import { join } from "path/posix";
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(express.static('public'));
+app.use(express.static('public/dist'));
+
 
 app.listen(port, ()=>{
     console.log('app listen on port' , port)
