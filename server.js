59cc8192a0aee507320e5df7486491a00fc08436
Tal <tal.yaron@gmail.com>
2021-08-20 10:05:17 +0300

    public static

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-app.get('/', function (req, res) {
-    res.send('<h1>I am alive!!!!!</h1>');
-});
+app.use(express.static('public'));
 app.listen(port, function () {
     console.log('app listen on port', port);
 });
--- server.ts
+++ server.ts
@@ -3,9 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/',(req, res)=>{
-    res.send('<h1>I am alive!!!!!</h1>')
-})
+app.use(express.static('public'));
 
 app.listen(port, ()=>{
     console.log('app listen on port' , port)
